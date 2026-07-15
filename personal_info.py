8270b8c1bfb0fa6fa40ab0cbfa5ea7fa7ae57a41
Joyce Vinluan <joyceannej@duck.com>
2026-07-14 21:49:47 -0400

    Initial commit

---
+++ personal_info.py
@@ -0,0 +1,7 @@
+#Lab 0 -Setup program displays student info for Joyce Vinluan
+print("Name: Joyce Vinluan")
+print("ID: 1977811")
+print ("Phone:301-266-3301")
+print("Department: Data Science")
+print("Grade: Graduate")
+print("Github link: https://github.com/jannevin13/PYCH01.git")
